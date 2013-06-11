d554268ba6f7bd429efcd0ea23ecb26aa354dac7
Philipp Klaus <philipp.klaus@gmail.com>
2013-06-11 14:03:56 +0200

    Making it backwards compatible with Python 2.7.x again

--- whatsmyip.py
+++ whatsmyip.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 """
@@ -11,7 +11,7 @@ Check <https://github.com/pklaus/WhatsMyIP> for newer versions.
 
 import time
 import socket
-import http.server
+import BaseHTTPServer
 
 
 #HOST_NAME = 'example.net'
@@ -19,7 +19,7 @@ HOST_NAME = '::'
 PORT_NUMBER = 8080
 
 
-class MyHandler(http.server.BaseHTTPRequestHandler):
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_HEAD(s):
         s.send_response(200)
         s.send_header("Content-type", "text/plain")
@@ -31,18 +31,18 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
         s.end_headers()
         ip = s.client_address[0]
         if ip.startswith('::ffff:'): ip = ip.split('::ffff:')[1]
-        s.wfile.write(ip.encode('ascii'))
+        s.wfile.write(ip)
 
-class HTTPServerV6(http.server.HTTPServer):
+class HTTPServerV6(BaseHTTPServer.HTTPServer):
     address_family = socket.AF_INET6
 
 if __name__ == '__main__':
     httpd = HTTPServerV6((HOST_NAME, PORT_NUMBER), MyHandler)
-    print(time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER))
+    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
     httpd.server_close()
-    print(time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER))
+    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
 
