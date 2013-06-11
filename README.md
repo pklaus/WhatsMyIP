7bb0481c21307d8433ff3f656c6c9d0eb9f3e443
Philipp Klaus <philipp.klaus@gmail.com>
2013-06-11 13:51:53 +0200

    Moving this project from a Gist to a full blown Github rep.

---
+++ README.md
@@ -0,0 +1,9 @@
+A simple HTTP Server written in Python:
+It returns the REMOTE_ADDR to connecting clients.
+
+Inspired by
+* <http://wiki.python.org/moin/BaseHttpServer>
+* and <http://www.righto.com/2011/02/ipv6-web-serving-with-arc-or-python.html>
+
+Written in 2013 by Philipp Klaus <philipp.l.klaus →AT→ web.de>.
+Check <https://github.com/pklaus/WhatsMyIP> for newer versions.
--- Remote-Address-HTTP-Server.py
+++ Remote-Address-HTTP-Server.py
@@ -5,12 +5,8 @@
 A simple HTTP Server written in Python:
 It returns the REMOTE_ADDR to connecting clients.
 
-Inspired by
-* http://wiki.python.org/moin/BaseHttpServer
-* and http://www.righto.com/2011/02/ipv6-web-serving-with-arc-or-python.html
-
-Written on 2013-02-18 by Philipp Klaus <philipp.l.klaus →AT→ web.de>.
-Check <https://gist.github.com/pklaus/4980542> for newer versions.
+Written in 2013 by Philipp Klaus <philipp.l.klaus →AT→ web.de>.
+Check <https://github.com/pklaus/WhatsMyIP> for newer versions.
 """
 
 import time
