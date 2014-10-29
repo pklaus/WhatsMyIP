8e675045835442a64cfa3e1fbd988e5f05ce31d7
Philipp Klaus <philipp.l.klaus@web.de>
2014-10-29 11:46:08 +0100

    new arg --consider-xrealip (& use it for logging)

--- whatsmyip.py
+++ whatsmyip.py
@@ -15,6 +15,8 @@ import http.server
 import ipaddress
 import urllib.parse
 
+CONSIDER_XREALIP = False
+
 class RequestHandler(http.server.BaseHTTPRequestHandler):
     """
     A request handler that can only return the
@@ -31,7 +33,9 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
         path = parsed_path[2]
         if path == '/':
             RequestHandler.do_HEAD(s)
-            address = s.headers.get('X-Real-IP')
+            address = None
+            if CONSIDER_XREALIP:
+                address = s.headers.get('X-Real-IP')
             if not address: address = s.client_address[0]
             ip = ipaddress.ip_address(address)
             if ip.version == 6 and ip.ipv4_mapped: ip = ip.ipv4_mapped
@@ -47,6 +51,16 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
             s.send_header("Content-type", "text/plain")
             s.end_headers()
             s.wfile.write("404 - Not found".encode('ascii'))
+    def address_string(s):
+        address = None
+        if CONSIDER_XREALIP:
+            address = s.headers.get('X-Real-IP')
+        if not address: address = s.client_address[0]
+        ip = ipaddress.ip_address(address)
+        if ip.version == 6 and ip.ipv4_mapped:
+            return "::ffff:" + str(ip.ipv4_mapped)
+        else:
+            return str(ip)
     def version_string(self):
         return 'WhatsMyIP/Python'
     def log_request(self, code='-', size='-'):
@@ -69,12 +83,17 @@ class HTTPServerV6(http.server.HTTPServer):
     address_family = socket.AF_INET6
 
 def main():
+    global CONSIDER_XREALIP
+
     import argparse
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('--host', default='::', help='IPv6 to bind to. Default is "::".')
     parser.add_argument('--port', default=8080, type=int, help='Port to listen at. Default is 8080.')
+    parser.add_argument('--consider-xrealip', action="store_true", help='If you want the header X-Real-IP to be considered, set this arg.')
 
     args = parser.parse_args()
+
+    if args.consider_xrealip: CONSIDER_XREALIP = True
 
     httpd = HTTPServerV6((args.host, args.port), RequestHandler)
     print(time.asctime(), "Server Starts - %s:%s" % (args.host, args.port))
